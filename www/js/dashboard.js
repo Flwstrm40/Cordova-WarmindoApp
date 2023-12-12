5c700b1a8193c460b05fe0ef5331d7c3f4cf187c
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2023-12-12 21:10:40 +0700

    add warung and transaction card

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -21,3 +21,40 @@ function readKaryawan() {
     window.location.href = '../karyawanList.html';
 }
 
+// Fungsi untuk mengefetch total warung
+async function getTotalWarung() {
+    try {
+      const response = await fetch('https://cordova-warmindo-api.vercel.app/total_warung');
+      const data = await response.json();
+      const jumlahWarungElement = document.getElementById('jumlah-warung');
+      jumlahWarungElement.textContent = data.total_warung;
+    } catch (error) {
+      console.error('Error fetching total warung:', error.message);
+      // Jika terjadi kesalahan, tampilkan pesan kesalahan
+      const jumlahWarungElement = document.getElementById('jumlah-warung');
+      jumlahWarungElement.textContent = 'Error fetching data';
+    }
+  }
+  
+  // Fungsi untuk mengefetch total transaksi
+  async function getTotalTransaksi() {
+    try {
+      const response = await fetch('https://cordova-warmindo-api.vercel.app/total_transaksi');
+      const data = await response.json();
+      const jumlahTransaksiElement = document.getElementById('jumlah-transaksi');
+      jumlahTransaksiElement.textContent = data.total_transaksi;
+    } catch (error) {
+      console.error('Error fetching total transaksi:', error.message);
+      // Jika terjadi kesalahan, tampilkan pesan kesalahan
+      const jumlahTransaksiElement = document.getElementById('jumlah-transaksi');
+      jumlahTransaksiElement.textContent = 'Error fetching data';
+    }
+  }
+  
+  // Panggil fungsi ini saat halaman dimuat
+  document.addEventListener('DOMContentLoaded', () => {
+    // Fetch total warung dan total transaksi
+    getTotalWarung();
+    getTotalTransaksi();
+  });
+
